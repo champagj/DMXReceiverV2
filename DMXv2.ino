d859a5e86cbe1070227f2d02658cceef266db272
JeffChampa <47355785+champagj@users.noreply.github.com>
2020-06-21 21:15:10 -0400

    Update DMXv2.ino

--- DMXv2.ino
+++ DMXv2.ino
@@ -6,8 +6,27 @@
 */
 
 //  include librayrys 
+#include <ESP8266WiFi.h>  
+#include <ESP8266mDNS.h>  
+#include <WiFiUdp.h>  
+#include <ArduinoOTA.h>  
+
+#include <FastLED.h>
+#include <DMXSerial.h>
 
 // define constant
+
+const char* ssid = "Champagne";       // SSID name
+const char* password = "1cafe2cafe";  // wifi password
+#define LED_PIN     12    /GPIO12
+#define NUM_LEDS    26
+#define LED_TYPE    WS2811
+#define COLOR_ORDER RGB
+//#define UPDATES_PER_SECOND  100
+CRGB leds[NUM_LEDS];
+#define DMXSTART 81  //First DMX Channel
+#define DMXLENGTH 88  // number of DMX channels used
+
 
 // Setup()
 
